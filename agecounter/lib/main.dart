3eb9e86c16041e517100e4d4c9660687cdb59662
BhavyaSontineni <bsontineni1@student.gsu.edu>
2025-02-24 14:13:05 -0500

    age milestones with msgs and colors

--- agecounter/lib/main.dart
+++ agecounter/lib/main.dart
@@ -1,260 +1,132 @@
 import 'dart:io' show Platform;
-
 import 'package:flutter/foundation.dart' show kIsWeb;
-
 import 'package:flutter/material.dart';
-
 import 'package:provider/provider.dart';
-
 import 'package:window_size/window_size.dart';
 
-
-
-
 void main() {
+  setupWindow();
 
-setupWindow();
-
-runApp(
-
-// Provide the model to all widgets within the app. We're using
-
-// ChangeNotifierProvider because that's a simple way to rebuild
-
-// widgets when a model changes. We could also just use
-
-// Provider, but then we would have to listen to Counter ourselves.
-
-//
-
-// Read Provider's docs to learn about all the available providers.
-
-ChangeNotifierProvider(
-
-// Initialize the model in the builder. That way, Provider
-
-// can own Counter's lifecycle, making sure to call dispose
-
-// when not needed anymore.
-
-create: (context) => Counter(),
-
-child: const MyApp(),
-
-),
-
-);
-
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => Counter(),
+      child: const MyApp(),
+    ),
+  );
 }
-
-
-
 
 const double windowWidth = 360;
-
 const double windowHeight = 640;
 
-
-
-
 void setupWindow() {
-
-if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
-
-WidgetsFlutterBinding.ensureInitialized();
-
-setWindowTitle('Provider Counter');
-
-setWindowMinSize(const Size(windowWidth, windowHeight));
-
-setWindowMaxSize(const Size(windowWidth, windowHeight));
-
-getCurrentScreen().then((screen) {
-
-setWindowFrame(Rect.fromCenter(
-
-center: screen!.frame.center,
-
-width: windowWidth,
-
-height: windowHeight,
-
-));
-
-});
-
+  if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
+    WidgetsFlutterBinding.ensureInitialized();
+    setWindowTitle('Age Milestone Counter');
+    setWindowMinSize(const Size(windowWidth, windowHeight));
+    setWindowMaxSize(const Size(windowWidth, windowHeight));
+    getCurrentScreen().then((screen) {
+      setWindowFrame(Rect.fromCenter(
+        center: screen!.frame.center,
+        width: windowWidth,
+        height: windowHeight,
+      ));
+    });
+  }
 }
-
-}
-
-
-
-
-/// Simplest possible model, with just one field.
-
-///
-
-/// [ChangeNotifier] is a class in flutter:foundation. [Counter] does
-
-/// _not_ depend on Provider.
 
 class Counter with ChangeNotifier {
+  int value = 0;
 
-int value = 0;
+  // Increment the counter and notify listeners
+  void increment() {
+    value += 1;
+    notifyListeners();
+  }
 
+  // Determine the milestone based on the current counter value
+  String get milestoneMessage {
+    if (value <= 12) {
+      return "You're a child!";
+    } else if (value <= 19) {
+      return "Teenager time!";
+    } else if (value <= 30) {
+      return "You're a young adult!";
+    } else if (value <= 50) {
+      return "You're an adult now!";
+    } else {
+      return "Golden years!";
+    }
+  }
 
-
-
-void increment() {
-
-value += 1;
-
-notifyListeners();
-
+  // Determine the background color based on the current counter value
+  Color get backgroundColor {
+    if (value <= 12) {
+      return Colors.lightBlue;
+    } else if (value <= 19) {
+      return Colors.lightGreen;
+    } else if (value <= 30) {
+      return Color(0xFFFFF8C2); 
+    } else if (value <= 50) {
+      return Colors.orange;
+    } else {
+      return Colors.grey[300]!;
+    }
+  }
 }
-
-}
-
-
-
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-const MyApp({super.key});
-
-
-
-
-@override
-
-Widget build(BuildContext context) {
-
-return MaterialApp(
-
-title: 'Flutter Demo',
-
-theme: ThemeData(
-
-primarySwatch: Colors.blue,
-
-useMaterial3: true,
-
-),
-
-home: const MyHomePage(),
-
-);
-
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Age Milestone Counter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
 }
-
-}
-
-
-
 
 class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
-const MyHomePage({super.key});
-
-
-
-
-@override
-
-Widget build(BuildContext context) {
-
-return Scaffold(
-
-appBar: AppBar(
-
-title: const Text('Flutter Demo Home Page'),
-
-),
-
-body: Center(
-
-child: Column(
-
-mainAxisAlignment: MainAxisAlignment.center,
-
-children: [
-
-const Text('You have pushed the button this many times:'),
-
-// Consumer looks for an ancestor Provider widget
-
-// and retrieves its model (Counter, in this case).
-
-// Then it uses that model to build widgets, and will trigger
-
-// rebuilds if the model is updated.
-
-Consumer<Counter>(
-
-builder: (context, counter, child) => Text(
-
-'${counter.value}',
-
-style: Theme.of(context).textTheme.headlineMedium,
-
-),
-
-),
-
-],
-
-),
-
-),
-
-floatingActionButton: FloatingActionButton(
-
-onPressed: () {
-
-// You can access your providers anywhere you have access
-
-// to the context. One way is to use Provider.of<Counter>(context).
-
-// The provider package also defines extension methods on the context
-
-// itself. You can call context.watch<Counter>() in a build method
-
-// of any widget to access the current state of Counter, and to ask
-
-// Flutter to rebuild your widget anytime Counter changes.
-
-//
-
-// You can't use context.watch() outside build methods, because that
-
-// often leads to subtle bugs. Instead, you should use
-
-// context.read<Counter>(), which gets the current state
-
-// but doesn't ask Flutter for future rebuilds.
-
-//
-
-// Since we're in a callback that will be called whenever the user
-
-// taps the FloatingActionButton, we are not in the build method here.
-
-// We should use context.read().
-
-var counter = context.read<Counter>();
-
-counter.increment();
-
-},
-
-tooltip: 'Increment',
-
-child: const Icon(Icons.add),
-
-),
-
-);
-
-}
-
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<Counter>(
+      builder: (context, counter, child) {
+        return Scaffold(
+          backgroundColor: counter.backgroundColor, // Update background color
+          appBar: AppBar(
+            title: const Text('Age Milestone Counter'),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('You have reached this milestone:'),
+                Text(
+                  counter.milestoneMessage,  // Show milestone message
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                Text(
+                  'Age: ${counter.value}', // Show current age (counter value)
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              counter.increment();
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+        );
+      },
+    );
+  }
 }
